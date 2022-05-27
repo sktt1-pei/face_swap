cdab9e4e6ccae8adcd04cd50628630826901f316
icemooo <13356340251@163.com>
2022-05-27 12:34:23 +0800

    基本能跑，但是有效点好像有bug，结果很奇怪

--- face_swap.py
+++ face_swap.py
@@ -46,6 +46,7 @@ def calculateDelaunayTriangles(rect,points):
 
     # Insert points into subdiv
     for p in points:
+        print(p)
         subdiv.insert(p)
     triangleList = subdiv.getTriangleList()
 
@@ -116,10 +117,13 @@ def warpTriangle(img1,img2,t1,t2):
 
 
 if __name__ =='__main__':
-    # Make sure Opencv is version or above
+    # Make sure Opencv is version or above jpg -> png
 
     filename1 = "sswap/1.png"
     filename2 = "sswap/1.jpg"
+
+
+
     filename3 = "sswap/mask.jpg"
     filename4 = "sswap/combina.jpg"
     filename5 = "sswap/swap.jpg"
@@ -160,6 +164,9 @@ if __name__ =='__main__':
     # Finde delanauy triangulation for convex hull points
     sizeImg2 = img1.shape
     rect = (0,0,sizeImg2[1],sizeImg2[0])
+
+    print(rect)
+    print(hull2)
 
     dt = calculateDelaunayTriangles(rect,hull2)
     if(len(dt) == 0):
@@ -204,9 +211,110 @@ if __name__ =='__main__':
 
 
 
+def face_swap_1(filename1, frame, points1, points2, valid_flg):
+    # trump -> jpg
+    '''
+    filename1 -> 1.jpg的脸
+    filename2 -> trump的脸
+    points1 -> 1.jpg的脸的点
+    points2 -> trump脸上光流形成的点
+    valid_flg -> 这些点是否有效 1 - 有效 0 - 无效
+    :return: 合成后的frame
+    '''
 
+    filename3 = "sswap/mask.jpg"
+    filename4 = "sswap/combina.jpg"
+    filename5 = "sswap/swap.jpg"
 
+    # 这是要换的那个男人的点集合
+    txt_path1 = "sswap/pointsjpg.txt"
+    #txt_path2 = "sswap/pointsjpg.txt"
 
+    img1 = cv2.imread(filename1)
+    img2 = frame
+    img1Warped = np.copy(img2)
+
+    # Read array of corresponding points
+    #points1 = readPoints(txt_path1)
+    #print(len(points1))
+    #points2 = readPoints(txt_path2)
+    #print(len(points2))
+
+    # 有效的点集合
+    points1_valid = []
+    points2_valid = []
+    for i in range(len(valid_flg)):
+        if(valid_flg[i] == 1):
+            points1_valid.append(points1[i])
+            points2_valid.append(points2[i])
+
+    # Find convex hull
+    hull1 = []
+    hull2 = []
+    img_corp = img1.copy()
+    hullIndex = cv2.convexHull(np.array(points2_valid), returnPoints=False)
+
+    # find convexHull
+    hullIndex1 = cv2.convexHull(np.array(points1_valid))
+    for i in range(len(hullIndex1)):
+        cv2.line(img_corp, tuple(hullIndex1[i][0]), tuple(hullIndex1[(i + 1) % len(hullIndex1)][0]), (255, 0, 0), 2)
+        # cv2.circle(img_corp,i,2,(205,0,0),2)
+    img_point = img1.copy()
+    for i in points1_valid:
+        cv2.circle(img_point, tuple(i), 2, (0, 255, 0), 5)
+    fillbox = np.hstack((img1, img_point, img_corp))
+    # cv2.imwrite("sswap/fillbox.png", fillbox)
+
+    for i in range(0, len(hullIndex)):
+        hull1.append(points1_valid[int(hullIndex[i])])
+        hull2.append(points2_valid[int(hullIndex[i])])
+
+    # Finde delanauy triangulation for convex hull points
+    sizeImg2 = img1.shape
+    rect = (0, 0, sizeImg2[1], sizeImg2[0])
+    print(rect)
+    print(hull2)
+    dt = calculateDelaunayTriangles(rect, hull2)
+    if (len(dt) == 0):
+        quit()
+
+    img2_con = img1.copy()
+    # Apply affine tranformation to Delaunay triangles
+    for i in range(0, len(dt)):
+        t1 = []
+        t2 = []
+
+        # Get points for img1,img2 corresponding to triangles
+        for j in range(0, 3):
+            t1.append(hull1[dt[i][j]])
+            t2.append(hull2[dt[i][j]])
+
+        for j in range(0, 3):
+            cv2.line(img2_con, t1[j], t1[(j + 1) % 3], (255, 0, 0), 2)
+
+        warpTriangle(img1, img1Warped, t1, t2)
+
+    out_convex = np.hstack((img1, img2_con))
+    # cv2.imwrite("sswap/fillbox_tri.png", out_convex)
+    # Calculate Mask
+    hull8U = []
+    for i in range(0, len(hull2)):
+        hull8U.append((hull2[i][0], hull2[i][1]))
+
+    mask = np.zeros(img2.shape, dtype=img2.dtype)
+    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
+
+    cv2.imwrite(filename3, mask)
+    cv2.imwrite(filename4, img1Warped)
+
+    r = cv2.boundingRect(np.float32([hull2]))
+    center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
+    output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
+    output_mask = np.hstack((img1Warped, img2, mask, output))
+    # cv2.imwrite("sswap/out_mask.png", output_mask)
+    #output = np.hstack((cv2.resize(img1, (img2.shape[0], img2.shape[1])), output, img2))
+    cv2.imwrite(filename5, output)
+    return output
 
 
 
--- optical_flow.py
+++ optical_flow.py
@@ -3,7 +3,7 @@ from time import sleep
 import numpy as np
 import cv2
 import get_point
-
+import face_swap
 
 # 所有有效的点
 valid = [i for i in range(68)]
@@ -35,14 +35,28 @@ p0 = get_point.getMousePos(old_gray)
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
+# 要换脸的那个人的脸的关键点集合 这个集合不会发生变化
+txt_path1 = "sswap/pointspng.txt"
+points1 = face_swap.readPoints(txt_path1)
+points2 = [(int(c[0][0]),int(c[0][1])) for c in p0]
+filename1 = "sswap/1.png"
+
 while (1):
+    # 对每一帧进行读取
     ret, frame = cap.read()
+    print(frame.shape)
+
+    # cv2.imshow(out)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None,
                                            **lk_params)
 
+
+
+
+    # 得到有效的点的集合
     for i in range(len(st)):
         if st[i] == 1:
             pass
@@ -51,15 +65,21 @@ while (1):
     for i in range(len(flg)):
         if(flg[i] == 0 and i in valid):
             valid.remove(i)
-    print((valid))
-    # Select good points
+    # ((valid))
+
+
+    for i in range(len(valid)):
+        points2[valid[i]] = (p1[i][0][0], p1[i][0][1])
+
+    out = face_swap.face_swap_1(filename1, frame, points1, points2, flg)
+    # Select good points 选择好的点
     try:
         good_new = p1[st == 1]
         good_old = p0[st == 1]
     except:
         pass
 
-    # draw the tracks
+    # draw the tracks 画出轨迹 没啥用
     for i, (new, old) in enumerate(zip(good_new, good_old)):
         a, b = new.ravel()
         c, d = old.ravel()
@@ -71,7 +91,7 @@ while (1):
         frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
     img = cv2.add(frame, mask)
 
-    cv2.imshow('frame', img)
+    cv2.imshow('frame', out)
     sleep(1/60)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
@@ -79,7 +99,8 @@ while (1):
 
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
+    # n * 1 * 2 表示点
     p0 = good_new.reshape(-1, 1, 2)
-
+    print(p0)
 cv2.destroyAllWindows()
 cap.release()
